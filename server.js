c4e2bf759d159298bf04993f4b10143f136014dd
faisalt <f.taher@lancaster.ac.uk>
2015-09-16 09:38:01 +0100

    Added Logging

--- server.js
+++ server.js
@@ -2,11 +2,11 @@
 * The node server handles client connections and message 
 * broadcasting (i.e. pushing messages to connected clients)
 *
-* @author Faisal T.
 */
 
-var port=3000;
 
+var port=3000;
+var fs = require('fs'); // For writing to filesystems.
 var app = io = require('socket.io').listen(app);
 
 //Listen on specified port
@@ -30,4 +30,20 @@ io.sockets.on('connection', function (socket) {
 		console.log(data);
         socket.broadcast.emit("LDR_VALUES", data);
     });
+	
+	socket.on("ACTION_LOG", function (data) {
+		console.log("logging data");
+		var parsed = JSON.parse(data);
+		logData(","+parsed.Timestamp+","+parsed.Participant+","+parsed.Action_Name+","+parsed.Action_Type);
+    });
 });
+
+
+/** Function to append log data to file. */
+function logData(data) {
+	data = data+"\r\n";
+	fs.appendFile('logs/study_log.txt', data, function (err) {
+		if(err) { return console.log("Could not write to file \r\n" + err); }
+		else { }
+	});
+}
